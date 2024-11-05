7128443f971c0867e4f6313f84e9d686767f9000
adib-the-noob <mdadib550@gmail.com>
2024-11-05 17:49:56 +0600

    constants added

---
+++ constants.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+    const double PI = 3.1416;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+     
+    std::cout << circumference << std::endl;
+
+    return 0;
+}
