d698acd6c914d9f612eaefb124b44bbb00e7be4f
adib-the-noob <mdadib550@gmail.com>
2024-11-05 18:34:40 +0600

    arithmatic added

---
+++ arithmatic_operator.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int main(){
+    int students = 20;
+
+    // increment 
+    students = students + 1; // 21
+    students++; // 22 
+    students+=1; // 23
+
+    std::cout << students << "\n";
+
+    // decrement
+    students = 20;
+    
+    students = students - 1; // 19
+    students--; // 18
+    students -= 1; // 17 
+
+    std::cout << students;
+
+    return 0;
+}
