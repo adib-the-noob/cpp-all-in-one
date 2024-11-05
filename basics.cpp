8603df6c47fea5a0569814440c9084d1cfeff478
adib-the-noob <mdadib550@gmail.com>
2024-11-05 11:33:36 +0600

    basic code added

---
+++ basics.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main(){
+    std::cout << "I like pizza" << std::endl;
+    // endl means end of line
+    
+    std::cout << "Its very tasty" << '\n';
+    // '\n' also means end of line
+    
+    std::cout << "HackerOne is a great platform" << std::endl;
+    return 0;
+}
