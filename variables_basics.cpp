60c3400c6e0d9282efcb7a5770934b0a09e2e86a
adib-the-noob <mdadib550@gmail.com>
2024-11-05 11:39:58 +0600

    variables added

---
+++ variables_basics.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main(){
+    int x = 23;
+    int y = 4;
+    int sum = x + y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+    
+    return 0;
+}
