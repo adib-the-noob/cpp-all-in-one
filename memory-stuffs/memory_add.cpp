2d9458701164f9eb216f13b4ab9ae438ddf2caf5
adib-the-noob <mohammed.adib1104@gmail.com>
2024-11-14 02:01:57 +0600

    memeory address stuffs

---
+++ memory-stuffs/memory_add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name = "bro";
+    int age = 21;
+    bool student = true;
+
+
+    // print memeory address
+    cout << &age << endl;
+}
