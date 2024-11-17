0697fcc942e5c03ace813aab006d51aeda259ed7
adib-the-noob <mohammed.adib1104@gmail.com>
2024-11-17 18:12:38 +0600

    stl basics added

--- stl-from-basics/vector_datatype.cpp
+++ stl-from-basics/vector_datatype.cpp
@@ -3,16 +3,22 @@
 
 using namespace std;
 
+void getVectorInfo(vector<int> v){
+
+	cout << "[=====[vector elements]=====]\n";
+	for (int element : v){
+		cout << element << " "; 	
+	}
+	cout << "\n[=====[================]=====]\n";
+};
+
 int main()
 {
 	vector<int> vector1 = {1, 3, 4};
-
 	
 	// priniting the elements
 	cout << vector1[0] << endl;
 	
-	cout << "======================" << endl;
-
 	// operations - u can;t push from the front directly!
 	vector1.push_back(5); // 4
 	vector1.push_back(6); // 5
@@ -21,11 +27,17 @@ int main()
 	vector1.push_back(9); // 8
 
 	// print all the elements 
-	for (int element : vector1){
-		cout << element << " "; 
-	}
 
-	cout << "\n======================" << endl;
+	getVectorInfo(vector1);
+
+	vector1.pop_back();
+	vector1.pop_back();
+	vector1.pop_back();
+
+	getVectorInfo(vector1);
+
+	vector1.shrink_to_fit();
+
 	cout << "Capacity is > " << vector1.capacity() << endl;
 	cout << "Now the size is > " << vector1.size() << endl;
 }
